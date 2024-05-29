256f94b11881b9b3aa28f80f0029238539fb415b
Ethan Kramer <ethankr234@gmail.com>
2024-05-28 22:15:19 -0400

    feat: add initial tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello World!</h1>
-      <p>The app has been created!</p>
+      <h1 className="text-3xl bg-red-600 text-white p-3">Hello World!</h1>
+      <p className="bg-amber-400 text-black p-3">The app has been created!</p>
     </div>
   )
 }
